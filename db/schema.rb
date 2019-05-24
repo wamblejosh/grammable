6a9ec16d2b57a19f1bfadbf716fcbf051fc2f587
Joshua Wamble <wamblejosh@gmail.com>
2019-05-24 23:58:06 +0000

    adding carrierwave

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_20_080025) do
+ActiveRecord::Schema.define(version: 2019_05_24_232450) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,14 @@ ActiveRecord::Schema.define(version: 2019_05_20_080025) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "picture"
     t.index ["user_id"], name: "index_grams_on_user_id"
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "picture"
   end
 
   create_table "users", force: :cascade do |t|
